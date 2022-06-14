b5204155590cb5a6fd1371f4bc4049432d8d09ff
Martinson <Peter.Martinson@christianacare.org>
2022-06-14 18:28:32 -0400

    Clean up the code

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ def add(first_term, second_term):
     result = first_term + second_term
     return result
 
+
 def subtract(first_term, second_term):
     result = first_term - second_term
     return result
-
--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,13 @@
 import calculator
 
+
 def test__add():
     a = 1
     b = 2
     expected_value = 3
     actual_value = calculator.add(a, b)
     assert actual_value == expected_value
+
 
 def test__subtract():
     a = 2
