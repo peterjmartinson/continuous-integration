f0e59df6f7bec19d9d149a433c7e05554c6b25e3
Martinson <Peter.Martinson@christianacare.org>
2022-06-14 18:08:39 -0400

    Add some functions

---
+++ calculator.py
@@ -0,0 +1,9 @@
+
+def add(first_term, second_term):
+    result = first_term + second_term
+    return result
+
+def subtract(first_term, second_term):
+    result = first_term - second_term
+    return result
+
---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+import calculator
+
+def test__add():
+    a = 1
+    b = 2
+    expected_value = 3
+    actual_value = calculator.add(a, b)
+    assert actual_value == expected_value
+
+def test__subtract():
+    a = 2
+    b = 1
+    expected_value = 1
+    actual_value = calculator.subtract(a, b)
+    assert actual_value == expected_value
